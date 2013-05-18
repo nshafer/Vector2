97eea360e1976bafa63d5acaf6a6cfb105ba6afb
Nathan Shafer / Hoven <nate@lotech.org>
2013-05-18 10:57:04 -0700

    Added files

---
+++ Vector2.lua
@@ -0,0 +1,135 @@
+Vector2 = Core.class()
+Vector2._type = "Vector2"
+
+Vector2.x = nil
+Vector2.y = nil
+
+function Vector2:init(x, y)
+	self.x = x
+	self.y = y or x
+
+	-- Add our custom overrides to the existing metatable
+	local mt = getmetatable(self)
+	mt.__tostring = Vector2.tostring
+	mt.__serialize = Vector2.serialize
+	mt.__concat = Vector2.concat
+	
+	mt.__add = Vector2.add
+	mt.__sub = Vector2.sub
+	mt.__mul = Vector2.mul
+	mt.__div = Vector2.div
+	mt.__unm = Vector2.unm
+	
+	mt.__eq = Vector2.eq
+end
+
+-- Special functions
+function Vector2.distance(a, b)
+	local res = b - a
+	return math.sqrt(res.x^2 + res.y^2)
+end
+
+function Vector2.distancePoint(v, x, y)
+	local res = Vector2.new(v.x - x, v.y - y)
+	return math.sqrt(res.x^2 + res.y^2)
+end
+
+-- Member methods
+function Vector2:distanceTo(b)
+	return Vector2.distance(self, b)
+end
+
+function Vector2:distanceToPoint(x, y)
+	return Vector2.distancePoint(self, x, y)
+end
+
+-- Overloaded operators
+function Vector2.tostring(v)
+	return v.x .. "," .. v.y
+end
+
+function Vector2.serialize(v)
+	return v
+end
+
+function Vector2.concat(a, b)
+	if type(a) == "string" then
+		return a .. Vector2.tostring(b)
+	elseif type(b) == "string" then
+		return Vector2.tostring(a) .. b
+	else
+		return Vector2.tostring(a) .. Vector2.tostring(b)
+	end
+end
+
+function Vector2.add(a, b)
+	local res = Vector2.new()
+	if type(a) == "number" then
+		res.x = a + b.x
+		res.y = a + b.y
+	elseif type(b) == "number" then
+		res.x = a.x + b
+		res.y = a.y + b
+	else
+		res.x = a.x + b.x
+		res.y = a.y + b.y
+	end
+	return res
+end
+
+function Vector2.sub(a, b)
+	local res = Vector2.new()
+	if type(a) == "number" then
+		res.x = a - b.x
+		res.y = a - b.y
+	elseif type(b) == "number" then
+		res.x = a.x - b
+		res.y = a.y - b
+	else
+		res.x = a.x - b.x
+		res.y = a.y - b.y
+	end
+	return res
+end
+
+function Vector2.mul(a, b)
+	local res = Vector2.new()
+	if type(a) == "number" then
+		res.x = a * b.x
+		res.y = a * b.y
+	elseif type(b) == "number" then
+		res.x = a.x * b
+		res.y = a.y * b
+	else
+		res.x = a.x * b.x
+		res.y = a.y * b.y
+	end
+	return res
+end
+
+function Vector2.div(a, b)
+	local res = Vector2.new()
+	if type(a) == "number" then
+		res.x = a / b.x
+		res.y = a / b.y
+	elseif type(b) == "number" then
+		res.x = a.x / b
+		res.y = a.y / b
+	else
+		res.x = a.x / b.x
+		res.y = a.y / b.y
+	end
+	return res
+end
+
+function Vector2.unm(v)
+	local res = Vector2.new()
+	res.x = -v.x
+	res.y = -v.y
+	return res
+end
+
+function Vector2.eq(a, b)
+	return a.x == b.x and a.y == b.y
+end
+
---
+++ main.lua
@@ -0,0 +1,45 @@
+-- Vector2 examples
+local v1 = Vector2.new(1,2)
+local v2 = Vector2.new(3,3)
+
+-- Supports tostring
+print("v1", v1)
+print("v2", v2)
+
+-- Supports concatenation
+print("v1: ["..v1.."]")
+
+-- Supports addition
+print("v1 + v2", v1 + v2)
+print("v1 + 1", v1 + 1)
+print("1 + v1", 1 + v1)
+
+-- Supports subtraction
+print("v1 - v2", v1 - v2)
+print("v1 - 1", v1 - 1)
+print("1 - v1", 1 - v1)
+
+-- Supports multiplication
+print("v1 * v2", v1 * v2)
+print("v1 * 1", v1 * 1)
+print("1 * v1", 1 * v1)
+
+-- Supports division
+print("v1 / v2", v1 / v2)
+print("v1 / 1", v1 / 1)
+print("1 / v1", 1 / v1)
+
+-- Supports unary minus (negative)
+print("-v1", -v1)
+
+-- Supports equality testing
+print("v1 == v2", v1 == v2)
+
+-- Supports inequality testing
+print("v1 ~= v2", v1 ~= v2)
+
+-- Supports distance calculations
+print("v1:distanceTo(v2)", v1:distanceTo(v2))
+
+-- Or arbitrary points
+print("v1:distanceToPoint(5,6)", v1:distanceToPoint(5,6))
